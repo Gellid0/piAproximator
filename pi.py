7c18dfcfa2bdb3ff9b511c4eab85ed1f0d541acd
Diogo Bazzanella Borges <82784442+Gellid0@users.noreply.github.com>
2022-02-14 16:09:18 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,17 +1,14 @@
 from cmath import pi, sqrt
 import math
 
-#n = 31531856
-n = 99999999
-r = 1
+n = 3 #número de lados do poligono inicial
+r = 1 #a distância entre o centro e o vértice
 while True:
     Ang = 360/n
-    ACmp = (180-Ang)/2
-    #Ares = 2*(math.cos(math.radians(Ang/2))*r)   
+    ACmp = (180-Ang)/2 
     Ares = (math.sin(math.radians(Ang))*r)/math.sin(math.radians(ACmp))
-    #print(str(math.cos(math.radians(Ang))) + " " + str(Ang))
     piT = (n*Ares)/2*r
     Aprox = math.pi/piT
     n+=1
 
-    print("o valor atual de N é " + str(n) + " o erro aprox do Pi Teórico é " + str(Aprox) + " e o valor de PiT é " + str(piT))
+    print("o valor atual de N é " + str(n) + " o erro aprox do Pi Teórico é " + str(Aprox) + " e o valor resultante é " + str(piT))
